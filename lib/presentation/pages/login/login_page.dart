2dc02d95dbb10da5fd269778172c72a3ce92aace
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-09-01 04:05:59 +0900

    add:login_page_init

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+
 /// 로그인 페이지
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
