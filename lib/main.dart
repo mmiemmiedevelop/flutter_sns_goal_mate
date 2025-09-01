5d4dde10c667f65bc6f4890157652dae345c6813
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-09-01 17:30:42 +0900

    feat: 회원가입 UI add : Image_Ppcer, ios & android 권한 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_princess/presentation/pages/home_page/home_page.dart';
+import 'package:flutter_princess/presentation/pages/login/login_page.dart';
+import 'package:flutter_princess/presentation/pages/sign_up_page/sign_up_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage()
+      home: const SignUpPage(),
     );
   }
 }
