786b8b68aa6dd4b2e2cc3228bf8d716c86fcb1e8
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-09-01 14:09:23 +0900

    Revert "Merge pull request #1 from mmiemmiedevelop/chore/lee-setting"

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_princess/presentation/pages/home_page/home_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // flutterfire configure 로 생성된 파일
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -17,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter test',
+      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(),
+      home: const HomePage()
     );
   }
 }
